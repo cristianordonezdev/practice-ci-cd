f5b11446aecfe61756e15ba516cc91fc492d9d72
Ramón, Cristian (RIS-CON) <chinorubikguitar@hotmail.com>
2025-07-09 12:50:19 -0600

    eslint errors caused

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,28 @@
-import Image from "next/image";
+"use client";
 
+import Image from "next/image";
+import { useEffect } from "react";
+
+const initial = () => {
+  console.log("Initial function called");
+  var unusedVariable = "This will cause an error";
+  let message = "hello";
+  if (message == "hello") {  // Using == instead of ===
+    console.log("Another console log");
+  }
+
+
+}
 export default function Home() {
+  
+  useEffect(() => {
+    initial();
+  }, []);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
 
-        <h1 className="text-8xl">CI/CD Hackathon Front</h1>
+        <h1 className="text-2xl">CI/CD Hackathon Front</h1>
        
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
@@ -20,7 +37,7 @@ export default function Home() {
             src="/file.svg"
             alt="File icon"
             width={16}
-            height={16}
+              height={16}
           />
           Learn
         </a>
